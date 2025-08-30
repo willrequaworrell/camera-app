7bf7712913940e613eab3021a20a86a5ca814ee3
Will Worrell <willrequaworrell@gmail.com>
2025-08-30 00:44:19 -0400

    add camera component and photo taking capability w/ button press

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,62 @@
-import { Redirect } from 'expo-router'
-import React from 'react'
-import { Text, View } from 'react-native'
-import { useCameraDevice, useCameraPermission } from 'react-native-vision-camera'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Redirect } from 'expo-router';
+import React, { useRef, useState } from 'react';
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Camera, TakePhotoOptions, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
+
+
 
 const HomeScreen = () => {
-  const {hasPermission} = useCameraPermission()
-  if (!hasPermission) {
-    return <Redirect href={"/permissions"}/>
+  const { hasPermission } = useCameraPermission()
+  const [photoSettings, setPhotoSettings] = useState<TakePhotoOptions>({
+    flash: "auto"
+  })
+  const camera = useCameraDevice('front')
+  const cameraRef = useRef<Camera | null>(null)
+
+  const takePhoto = async () => {
+
+    try {
+      if (!cameraRef) throw new Error("Failed to access camera!")
+      console.log("Taking photo")
+      const photo = await cameraRef.current?.takePhoto(photoSettings)
+      console.log("photo path:", photo!.path )
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
 
-  const camera = useCameraDevice("front")
+  if (!hasPermission) {
+    return <Redirect href="/permissions" />
+  }
+
+  if (!camera) {
+    return <SafeAreaView className='flex-1 bg-slate-900'/>
+  }
+
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Hello World
-      </Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-slate-900'>
+      <View className='p-4 h-full'>
+        <View className='relative h-[85%] rounded-2xl overflow-hidden'>
+          <Camera
+            ref={cameraRef}
+            device={camera}
+            isActive={true}
+            photo={true}                     
+            style={StyleSheet.absoluteFill}  
+          />
+        </View>
+        <View className='flex-row justify-center items-center flex-1 '>
+          <TouchableOpacity onPress={takePhoto}>
+            <View className='flex justify-center items-center border-white border-2 rounded-full size-24'>
+              <FontAwesome name="circle" size={72} color="#7E22CD" />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
